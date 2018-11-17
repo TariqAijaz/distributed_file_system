746ac2ceb2b23a6059f8b76c275bc1241603214e
TariqAijaz <tariq.aijaz99@gmail.com>
2018-11-18 01:42:07 +0500

    make file, make directory, list files

--- client.py
+++ client.py
@@ -25,6 +25,10 @@
 import socket
 import sys
 import os
+import json
+import pickle
+import pprint
+from functools import reduce
 
 def intialize_socket(host,port):
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -35,65 +39,142 @@ def intialize_socket(host,port):
         sys.exit()
     return soc
 
+# def get_directory_structure(rootdir):
+#     """
+#     Creates a nested dictionary that represents the folder structure of rootdir
+#     """
+#     dir = {}
+#     rootdir = rootdir.rstrip(os.sep)
+#     start = rootdir.rfind(os.sep) + 1
+#     for path, dirs, files in os.walk(rootdir):
+#         dirs[:] = [d for d in dirs if d not in ['_pycache_','.git']]
+#         folders = path[start:].split(os.sep)
+#         value = {'Type':'F', 'Version': 1}
+#         subdir = dict.fromkeys(files, value)
+#         parent = reduce(dict.get, folders[:-1], dir)
+#         #print(parent)
+#         parent[folders[-1]] = subdir
+#     return dir
+
 
 def main():
     host = input(" -> IP: ")
     port = int(input(" -> Port: "))
-    # downloadDir = "E:/IBA-CS FALL 2018 (CS-8)/Distributed Operating System/final_project"
 
     soc = intialize_socket(host,port)
 
+    # filess = {}
+    # dictionary = {}
+
+    # BASE_PATH = os.path.realpath(os.getcwd())
+    # print(BASE_PATH)
+    # filess = get_directory_structure(BASE_PATH)
+
+    # host = 'localhost'
+    # port = 30000
+    # connections = {'30001' : {'ip':'127.0.0.0','port':30001}, '30002' : {'ip':'127.0.0.0','port':30002}}
+
+    # thing = BASE_PATH+'/config.txt'
+    # print(thing)
+    # dictionary = {'host' : host, 'port' : port, 'connections':connections}
+
+    # with open(thing, 'w') as fp:
+    #     json.dump(dictionary, fp, indent=4)
+
+    # with open(BASE_PATH+'/files.txt', 'w') as f:
+    #     json.dump(filess, f, indent=4)
+
+
+    # with open(thing, 'r') as fp:
+    #     data = json.load(fp)
+    #     print(str(data))
+
+    # print(filess)
+    # print(dictionary)
+    path = "root"
     while True:
         message = input(" -> ")
         if message == 'exit':
-            soc.sendall(message.encode("utf8"))
+            commands = []
+            commands.append(message)
+            data = pickle.dumps(commands)
+            soc.sendall(data)
             print("Client Closed")
             soc.close()
             sys.exit()
 
-        elif message == 'dir':
-            soc.sendall(message.encode("utf8"))
-            recv_input = soc.recv(5120).decode("utf8")
-            print(recv_input)
+        elif message == 'ls':
+            commands = []
+            commands.append(message)
+            commands.append(path)
+            data = pickle.dumps(commands)
+            soc.sendall(data)
+            recv_files_list = soc.recv(5120)
+            decoded_input = pickle.loads(recv_files_list)
+            for files in decoded_input:
+                print(files)
+
+        elif message[:5] == "mkdir":
+            commands = []
+            commands.append(message[:5])
+            commands.append(message[6:])
+            commands.append(path)
+            data = pickle.dumps(commands)
+            soc.sendall(data)
+            recv_message = soc.recv(5120).decode("utf8")
+            print(recv_message)
         
-        elif message[:2] == 'cd':
-            soc.sendall(message.encode("utf8"))
-            recv_input = soc.recv(5120).decode("utf8")
-            print(recv_input)
+        elif message[:6] == "mkfile":
+            commands = []
+            commands.append(message[:6])
+            commands.append(message[7:])
+            commands.append(path)
+            data = pickle.dumps(commands)
+            soc.sendall(data)
+            recv_message = soc.recv(5120).decode("utf8")
+            print(recv_message)
 
-        elif message[:8] == "download":
-            filename = message[9:]
-            soc.sendall(message.encode("utf8"))
-            with open("new_"+filename, 'wb') as file_to_write:
-                data = soc.recv(5120).decode("utf8")
-                if not data:
-                    break
-                file_to_write.write(data.encode())
-                print("Download Complete")
-                file_to_write.close()
+        # elif message[:2] == 'cd':
+        #     soc.sendall(message.encode("utf8"))
+        #     recv_input = soc.recv(5120).decode("utf8")
+        #     print(recv_input)
 
-        elif message[:6] == "create":
-            soc.sendall(message.encode("utf8"))
-            recv_input = soc.recv(5120).decode("utf8")
-            print(recv_input)
+        # elif message[:8] == "download":
+        #     filename = message[9:]
+        #     soc.sendall(message.encode("utf8"))
+        #     with open("new_"+filename, 'wb') as file_to_write:
+        #         data = soc.recv(5120).decode("utf8")
+        #         if not data:
+        #             break
+        #         file_to_write.write(data.encode())
+        #         print("Download Complete")
+        #         file_to_write.close()
+
+        # elif message[:6] == "create":
+        #     soc.sendall(message.encode("utf8"))
+        #     recv_input = soc.recv(5120).decode("utf8")
+        #     print(recv_input)
         
-        elif message[:4] == "read":
-            soc.sendall(message.encode("utf8"))
-            recv_input = soc.recv(5120).decode("utf8")
-            print(recv_input)   
+        # elif message[:4] == "read":
+        #     soc.sendall(message.encode("utf8"))
+        #     recv_input = soc.recv(5120).decode("utf8")
+        #     print(recv_input)   
 
-        elif message[:6] == "update":
-            soc.sendall(message.encode("utf8"))
-            recv_input = soc.recv(5120).decode("utf8")
-            print(recv_input)   
+        # elif message[:6] == "update":
+        #     soc.sendall(message.encode("utf8"))
+        #     recv_input = soc.recv(5120).decode("utf8")
+        #     print(recv_input)   
 
-            data = input("-> Enter Data in the file: ")
-            soc.sendall(data.encode("utf8"))
-            recv_input1 = soc.recv(5120).decode("utf8")
-            print(recv_input1)   
+        #     data = input("-> Enter Data in the file: ")
+        #     soc.sendall(data.encode("utf8"))
+        #     recv_input1 = soc.recv(5120).decode("utf8")
+        #     print(recv_input1)   
 
         else:
-            soc.sendall(message.encode("utf8"))
+            commands = []
+            commands.append(message[:])
+            data = pickle.dumps(commands)
+            soc.sendall(data)
             recv_input = soc.recv(5120).decode("utf8")
             print(recv_input)
 
--- fileserver.py
+++ fileserver.py
@@ -32,121 +32,252 @@ import sys
 import traceback
 import os
 import pickle
-from threading import Thread
+import json
+from threading import Thread, ThreadError
 
-
+# config_file = {
+#                   "Server_A": {
+#                   "ip": "localhost",
+#                   "port": 30000
+#                   },
+#                   "Server_B":  {
+#                   "ip": "localhost",
+#                   "port": 30001
+#                   },
+#                   "Server_C": {
+#                   "ip": "localhost",
+#                   "port": 30002
+#                   }
+#             }
 def main():
     start_server()
 
+def read_connection_from_config_file():
+    f = open('configA.txt', 'r')
+    dicte = json.load(f)
+    connections = dicte['connections']
+    sock = []
+    for server, connection in connections.items():
+        sock.append(connect_to_servers(connection['ip'],connection['port']))
+    return sock
+
+def connect_to_servers(host, port):
+    try:
+        sockets = socket.create_connection((host,port))
+        print("Connected To Server")
+        return sockets
+    except:
+        print("Connection error")
+    
 
 def start_server():
     host = "127.0.0.1"
-    # port = 8888
-    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)   # SO_REUSEADDR flag tells the kernel to reuse a local socket in TIME_WAIT state, without waiting for its natural timeout to expire
-    print("Socket created")
-    # port = soc.getsockname()[1]
-    # print('listening on port:', soc.getsockname()[1])
-
+    port = 30000
     try:
-        soc.bind((host, 0))
-    except:
-        print("Bind failed. Error : " + str(sys.exc_info()))
+        sock = read_connection_from_config_file()
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        soc.bind((host, port))
+        print("Socket created")
+        print("Socket now listening")
+        print('listening on port:', soc.getsockname()[1])
+        soc.listen(5)  
+        while True:
+            connection, address = soc.accept()
+            ip, port = str(address[0]), str(address[1])
+            print("Connected with " + ip + ":" + port)
+            Thread(target=client_thread, args=(connection, ip, port)).start()
+        # soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)   # SO_REUSEADDR flag tells the kernel to reuse a local socket in TIME_WAIT state, without waiting for its natural timeout to expire
+        
+    except (socket.error, ThreadError) as e:
+        print("Socket Error: " + str(e))
         sys.exit()
 
-    soc.listen(5)       # queue up to 5 requests
-    print("Socket now listening")
-    print('listening on port:', soc.getsockname()[1])
-
     # infinite loop- do not reset for every requests
-    while True:
-        connection, address = soc.accept()
-        ip, port = str(address[0]), str(address[1])
-        print("Connected with " + ip + ":" + port)
-
-        try:
-            Thread(target=client_thread, args=(connection, ip, port)).start()
-        except:
-            print("Thread did not start.")
-            traceback.print_exc()
-
-    soc.close()
-
 
 def client_thread(connection, ip, port, max_buffer_size = 5120):
     is_active = True
-
+    path = "root"
     while is_active:
-        client_input = receive_input(connection, max_buffer_size)
+        try:
+            client_input = receive_input(connection, max_buffer_size)
 
-        if "exit" in client_input:
-            print("Client is requesting to disconnect")
-            connection.close()
-            print("Connection " + ip + ":" + port + " closed")
-            is_active = False
+            if "exit" in client_input[0]:
+                print("Client is requesting to disconnect")
+                connection.close()
+                print("Connection " + ip + ":" + port + " closed")
+                is_active = False
+                break
 
-        elif "dir" in client_input:
-            directory = os.listdir()
-            data = '\n'.join(directory)
-            connection.sendall(data.encode("utf8"))
-        
-        elif "cd" in client_input:
-            directory = client_input[3:]
-            directory_name = os.path.realpath(directory)
-            change_directory = os.chdir(directory_name)
-            data = 'Directory Changed'
-            connection.sendall(data.encode("utf8"))
-
-        elif client_input[:8] == "download":
-            filename = client_input[9:]
-            if os.path.isfile(filename):
-                with open(filename, 'rb') as file_to_send:
-                    bytesToSend = file_to_send.read(5120)
-                    connection.sendall(bytesToSend)
-                    while bytesToSend != "":
-                        if bytesToSend != "":
-                            break
-                        bytesToSend = file_to_send.read(5120)
-                        connection.sendall(bytesToSend)
-        
-        elif client_input[:6] == "create":
-            filename = client_input[7:]
-            open(filename, 'w+')
-            msg = 'File Created' 
-            connection.sendall(msg.encode("utf8"))
-
-        elif client_input[:4] == "read":
-            filename = client_input[5:]
-            f = open(filename, 'rb')
-            if f.mode == 'rb':
-                data = f.read()
+            elif "ls" in client_input[0]:
+                files_list = list_directory(path)
+                data = pickle.dumps(files_list)
                 connection.sendall(data)
-        
-        elif client_input[:6] == "update":
-            filename = client_input[5:]
-            f = open(filename, 'a')
-            msg = "File Opened!"
-            connection.sendall(msg.encode("utf8"))
-            data = f.write(client_input)
-            msg1 = 'File Updated'
-            # if f.mode == 'rb':
-            #     data = f.read()
-            connection.sendall(msg1.encode("utf8"))
+            
+            elif "mkdir" in client_input[0]:
+                message = make_directory(client_input[1], client_input[2])
+                connection.sendall(message.encode("utf8"))
+            
+            elif "mkfile" in client_input[0]:
+                message = make_file(client_input[1], client_input[2])
+                connection.sendall(message.encode("utf8"))
 
-        else:
-            print("Processed result: {}".format(client_input))
-            connection.sendall(client_input.encode("utf8"))
+            # elif "cd" in client_input[0]:
+            #     message = make_file(client_input[1], client_input[2])
+            #     connection.sendall(message.encode("utf8"))
+            
+            # elif "cd" in client_input:
+            #     directory = client_input[3:]
+            #     directory_name = os.path.realpath(directory)
+            #     change_directory = os.chdir(directory_name)
+            #     data = 'Directory Changed'
+            #     connection.sendall(data.encode("utf8"))
 
+            # elif client_input[:8] == "download":
+            #     filename = client_input[9:]
+            #     if os.path.isfile(filename):
+            #         with open(filename, 'rb') as file_to_send:
+            #             bytesToSend = file_to_send.read(5120)
+            #             connection.sendall(bytesToSend)
+            #             while bytesToSend != "":
+            #                 if bytesToSend != "":
+            #                     break
+            #                 bytesToSend = file_to_send.read(5120)
+            #                 connection.sendall(bytesToSend)
+            
+            # elif client_input[:6] == "create":
+            #     filename = client_input[7:]
+            #     open(filename, 'w+')
+            #     msg = 'File Created' 
+            #     connection.sendall(msg.encode("utf8"))
+
+            # elif client_input[:4] == "read":
+            #     filename = client_input[5:]
+            #     f = open(filename, 'rb')
+            #     if f.mode == 'rb':
+            #         data = f.read()
+            #         connection.sendall(data)
+
+            # elif client_input[:4] == "read":
+            #     filename = client_input[5:]
+            #     f = open(filename, 'rb')
+            #     if f.mode == 'rb':
+            #         data = f.read()
+            #         connection.sendall(data)
+            
+            # elif client_input[:6] == "update":
+            #     filename = client_input[5:]
+            #     f = open(filename, 'a')
+            #     msg = "File Opened!"
+            #     connection.sendall(msg.encode("utf8"))
+            #     data = f.write(client_input)
+            #     msg1 = 'File Updated'
+            #     # if f.mode == 'rb':
+            #     #     data = f.read()
+            #     connection.sendall(msg1.encode("utf8"))
+
+            else:
+                message = "Invalid Request"
+                connection.sendall(message.encode("utf8"))
+        except (ThreadError, socket.error) as e:
+            print('Thread Error: {}'.format(e))
+            connection.close()
+            return
 
 def receive_input(connection, max_buffer_size):
     client_input = connection.recv(max_buffer_size)
     client_input_size = sys.getsizeof(client_input)
-
     if client_input_size > max_buffer_size:
         print("The input size is greater than expected {}".format(client_input_size))
 
-    decoded_input = client_input.decode("utf8").rstrip()  # decode and strip end of line
+    decoded_input = pickle.loads(client_input)
+    # decoded_input = client_input.decode("utf8").rstrip()  # decode and strip end of line
     return decoded_input
+
+
+def list_directory(path):
+    startpath = os.getcwd()
+    root = startpath+'\\root'
+    values = []
+    path_component = {}
+    for p in path:
+        path_component = path.split('\\')
+    f = open(root+'\\files.json', 'r')
+    structure = json.load(f)
+    dicte = {}
+    for comp in path_component:
+        if comp in structure.keys():
+                structure = structure[comp]['children']
+    for value in structure.keys():
+        values.append(value)
+    return values
+
+def make_directory(folder_name, path):
+    startpath = os.getcwd()
+    root = startpath+'\\root'
+    directory_structure = { folder_name: {
+        'name':folder_name,
+        'type':'directory',
+        'children':{}
+        }
+    }
+    path_component = {}
+    for p in path:
+        path_component = path.split('\\')
+    with open(root+'\\files.json', 'r') as f:
+        prev_structure = json.load(f)
+    structure = prev_structure
+    for comp in path_component:
+        if comp in structure.keys():
+                structure = structure[comp]['children']
+
+    if folder_name in structure.keys():
+        message = 'Folder Exists!'
+    else:
+        structure.update(directory_structure)
+        message = "Directory Created"
+
+    for key in prev_structure:
+        if key in structure:         
+            prev_structure[key].update(structure[key])
+
+    with open(root+'\\files.json', 'w') as f:
+        json.dump(prev_structure, f, indent=4)
+    
+    return message
+
+def make_file(file_name, path):
+    startpath = os.getcwd()
+    root = startpath+'\\root'
+    file_name = { file_name: {
+        'name': file_name,
+        'type':'file',
+        }
+    }
+    
+    path_component = {}
+    for p in path:
+        path_component = path.split('\\')
+    with open(root+'\\files.json', 'r') as f:
+        prev_structure = json.load(f)
+    structure = prev_structure
+    for comp in path_component:
+        if comp in structure.keys():
+                structure = structure[comp]['children']
+    
+    if file_name in structure.values():
+        message = 'File Exists!'
+    else:
+        structure.update(file_name)
+        message = "File Created"
+
+    for key in prev_structure:
+        if key in structure:         
+            prev_structure[key].update(structure[key])
+
+    with open(root+'\\files.json', 'w') as f:
+        json.dump(prev_structure, f, indent=4)
+
+    return message
 
 if __name__ == "__main__":
     main()
